2613db82f12969d64d5ee6ebfa26e0699f32833f
1v17 <166570730+1v17@users.noreply.github.com>
2024-10-07 16:08:05 -0700

    add labels and button to activity entry screen

--- screens/ActivityEntry.js
+++ screens/ActivityEntry.js
@@ -1,12 +1,44 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 import React from 'react'
+import { useContext } from 'react'
+
+import ScreenBackground from '../components/ScreenBackground'
+import Colors from '../constants/Colors'
+import { ItemsContext } from '../context/ItemsContext'
+import { ThemeContext } from '../context/ThemeContext'
 
 const ActivityEntry = () => {
+
+  const { addActivityItem } = useContext(ItemsContext);
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <View>
-      <Text>ActivityEntry</Text>
-    </View>
+    <ScreenBackground>
+      <View style={styles.inputSection} >
+        <Text>Activity*</Text>
+        <Text>Duration (min)*</Text>
+      </View>
+      <View style={styles.buttonSection} >
+        <Button 
+          title="Save" 
+          onPress={() => alert('Activity Added')}
+          color={Colors.primaryColor}
+        />
+      </View>
+    </ScreenBackground>
   )
 }
+
+const styles = StyleSheet.create({
+  inputSection: {
+    flex: 3,
+    justifyContent: 'center',
+    alignContent: 'flex-start',
+    padding: '5%',
+  },
+  buttonSection: {
+    flex: 1,
+  },
+});
 
 export default ActivityEntry
