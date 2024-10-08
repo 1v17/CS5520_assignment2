4c75f273463470a5e519184b1cb8759f0b350da5
1v17 <166570730+1v17@users.noreply.github.com>
2024-10-08 15:40:01 -0700

    add a special icon in item component

--- components/Item.js
+++ components/Item.js
@@ -1,15 +1,17 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { useContext } from 'react'
+import Entypo from '@expo/vector-icons/Entypo';
 
 import { ThemeContext } from '../context/ThemeContext'
-import Colors from '../constants/Colors'
 
 const Item = ({item, type}) => {
 
   const { theme } = useContext(ThemeContext);
   const formattedDate = new Date(item.date).toDateString();
-
+  const showSpecialMark = (type === "activity") ? 
+    ((item.name === "Running" || item.name === "Weights") && Number(item.duration) > 60) :
+    (Number(item.calories) > 800);
     return (
       <View style={[styles.container, 
         {backgroundColor: theme.foregroundColor,}]} >
@@ -17,6 +19,7 @@ const Item = ({item, type}) => {
           {(type === "activity") ? item.name : item.description}
         </Text>
         <View style={styles.rightView} >
+          {showSpecialMark && <Entypo name="warning" size={20} color={theme.backgroundColor} />}
           <View style={[styles.textBox, {backgroundColor: theme.backgroundColor}]} >
             <Text style={[styles.text, {color: theme.primaryColor}]} >
               {formattedDate}
@@ -44,20 +47,23 @@ const styles = StyleSheet.create({
   },
   rightView: {
     flexDirection: 'row',
-    justifyContent: 'flex-end',
     gap: 10,
+    justifyContent: 'center', 
     alignContent: 'center',
+    alignItems: 'center',
   },
   label: {
-    fontSize: 17,
-    paddingVertical: "1%",
+    fontSize: 15,
+    paddingVertical: "2%",
     fontWeight: 'bold',
   },
   textBox: {
     borderRadius: 5,
+    justifyContent: 'center',
+    alignContent: 'center',
   },
   text: {
-    fontSize: 15,
+    fontSize: 13,
     paddingVertical: "1%",
     paddingHorizontal: "2%",
   },
